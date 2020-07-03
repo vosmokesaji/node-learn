dde33aa14955bdb9b9e300004ec58f51f040f1a1
FreyaLin <690610702@qq.com>
2020-07-03 14:56:46 +0800

    feat(src/middleware): 中间件 - 使用中间件 init

---
+++ src/middleware/useing-middleware.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+
+
+
+app.get('/', function (req, res) {
+    res.send("你好 中间件");
+})
+
+app.listen(3000, function () {
+    console.log("Example app listen on port 3000!");
+    console.log("Hold ⌘ and click http://localhost:3000/ to visit ");
+})
