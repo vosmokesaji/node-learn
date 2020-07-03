9fcdf5c1e4fefddb5ab2347355f4880356ca859d
FreyaLin <690610702@qq.com>
2020-07-03 11:29:25 +0800

    feat(src/middleware): 添加第一个中间件 - 日志

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,9 +1,21 @@
 const express = require("express")
 const app = express()
 
+const myLogger = function(req,res,next){
+    console.log("logged");
+    next()
+}
+app.use(myLogger);
+
+// logger 必须在它上边，才能输出 logged
 app.get('/', function (req, res) {
     res.send("你好，中间件！")
 })
+
+
+// 中间件加载顺序非常重要：首先加载的中间件功能也首先执行。
+// 如果myLogger在到根路径的路由之后加载，请求将永远达不到它，并且应用程序不会打印“LOGGED”，因为根路径的路由处理程序会终止请求 - 响应循环。
+
 
 app.listen(3000, function () {
     console.log("Example app listen on port 3000!");
